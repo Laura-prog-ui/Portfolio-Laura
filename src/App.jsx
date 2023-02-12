9bd129e60397a3d879ed59a076a823e506ded20d
Laura-prog-ui <duarteriveralauraestefania0@gmail.com>
2023-02-11 19:58:42 -0500

    Header listo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css'
 import Header from './components/Header'
+import Home from './components/home/Home';
 
 function App() {
 
   return (
-    <div className="App">
-      <Header/>
-    </div>
+    <>
+      <Header />
+      <main className='main'>
+        <Home/>
+      </main>
+    </>
   )
 }
 
